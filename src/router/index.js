c8a2c239c1c632670918e2398a02cebfc028e1bd
lavikara <karatemitayo@gmail.com>
2019-11-08 16:42:58 +0100

    feat: created routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,35 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import Home from '../views/Home'
+import SignupPage from '../views/SignupPage'
+import TrackerPage from '../views/TrackerPage'
+import Result from '../views/Result'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
+export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/sign-up-page',
+      name: 'sign-up-page',
+      component: SignupPage
+    },
+    {
+      path: '/tracker-page',
+      name: 'tracker-page',
+      component: TrackerPage
+    },
+    {
+      path: '/result',
+      name: 'result',
+      component: Result
+    }
+  ]
 })
-
-export default router
