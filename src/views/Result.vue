0ed66287727abe052a29beb6b12806c82bdd2a1e
lavikara <karatemitayo@gmail.com>
2019-11-15 08:40:58 +0100

    added more display msg on result page

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -82,12 +82,26 @@
       </svg>
 
       <div class="baller">
-        <p>Baller!</p>
+        <p class="display-none the-money-monk">The Money Monk</p>
+        <p class="display-none the-essentialist">The Essentialist</p>
+        <p class="display-none the-sensible-sage">The Sensible Sage</p>
+        <p class="display-none the-baby-baller">The Baby Baller</p>
+        <p class="display-none the-spontanious-spender">The Spontaneous Spender</p>
+        <p class="display-none the-big-spender">The Big Spender</p>
+        <p class="display-none the-generous-sage">The Generous Sage</p>
+        <p class="display-none the-money-maestro">The Money Maestro</p>
       </div>
       <div class="comment">
-        <p>You love to have access to your money, whenever and wherever, we think you’d be a perfect match for our solution, join the waitlist!</p>
+        <p class="display-none the-money-monk-comment">You'd watch your money like a hawk if you could! It's no surprise to you that most of your money goes towards `highest expense` *.* In your bank, every kobo has it's place and every Naira has it's time. Do it easier with GoMoney. GoMoney is designed to help you keep track of your money. You get push notifications every time you spend; clear and concise spending overviews with categorised spending reports, a bill splitting tool and many channels through which you can receive (and send) money.</p>
+        <p class="display-none the-essentialist-comment">You probably spend a lot of time working out budgets and monitoring your spending habits; adjusting your needs to fit into your money goals. Most of your expenses go towards `highest expense` ; you're careful, not boring. GoMoney is designed to help you organise your money and declutter your finances, freeing you up to do more of the things you enjoy. So sit back, take a sip out of your (reasonably priced) drink and let GoMoney handle the hassle.</p>
+        <p class="display-none the-sensible-sage-comment">You have a budget, but you probably often go beyond it for `highest expense` and other expenses because you cannot come and die. It's not that you're bad at planning, you just haven't taken your needs and personality into account when creating your budget. GoMoney can help. You can monitor your expenses without spending too much time mapping out a budget. Your app will categorise your spending, so you see exactly how you are spending your money and can adjust to satisfy your desired goals.</p>
+        <p class="display-none the-baby-baller-comment">You like to plot your enjoyment; you know most of your money is going towards `highest expense` and you don't let mislay expenses steal your fun. Don't get caught up on calculating spending (when you should be chilling with the money), Let GoMoney handle the boring bits. Split bills, schedule payments, request and send money, and track expenses; with GoMoney, you can plan before, during and after you spend without breaking a sweat!</p>
+        <p class="display-none the-spontanious-spender-comment">Money goes through your account like water goes through a sponge— quickly, but languid enough that there's still some left for later. `highest expense` might claim most of your money now, but with you, that won't always be the case. Baller, meet GoMoney, flexibility is our MO. Think of GoMoney as your financial assistant, designed to tailor itself to your specific needs. Whether you're a keen saver, a big spender or somewhere in-between, we've got the tools to meet your needs.</p>
+        <p class="display-none the-big-spender-comment">You most likely have savings, but you're also not afraid to spend your money on the things you enjoy— like `highest expense`. You're what the kids call 'goals'. Let's make that easier. With GoMoney's payments features (plural), you can send and receive money from anyone, anytime, with as little as a unique payment link and their phone number. And with our ring-fenced security, you have total peace of mind knowing that someone is looking out for you while you're out enjoying your money.</p>
+        <p class="display-none the-generous-sage-comment">You're into using your money on the to buy yourself things you enjoy. For you, the thrill is sometimes in the spending, or the investing; GoMoney can fire up that fun. GoMoney has democratised payments, and in this regime, you choose what payment method works for you. Add bill splitting, scheduling payments, push notifications and a detailed expense tracker to the mix, you have all the tools you need to splurge till you drop.</p>
+        <p class="display-none the-money-maestro-comment">You are "baller" personified. Your mother birthed you to enjoy, and enjoy you shall. But how do you enjoy your money with all the limitations, security concerns and many account numbers flying around? Try GoMoney. With a variety of payment plans to choose from, you decide how and when you want to send money, depending on what you're feeling. And if your money goals change, no problem. GoMoney is designed to grow with you and help you achieve your goals!</p>
       </div>
-      <div class="amount" v-for="(detail, index) in amountBreakDown" :key="index">
+      <div class="amount" v-for="(detail, index) in amountSum" :key="index">
         <div class="figure">
           <p>₦{{ detail.value.toLocaleString() }}</p>
         </div>
@@ -133,7 +147,7 @@ export default {
   data() {
     return {
       userDetails: [],
-      amountBreakDown: [],
+      amountSum: [],
       selected: "Monthly",
       weekly: "",
       monthly: "",
@@ -142,13 +156,13 @@ export default {
   },
   mounted() {
     this.userDetails = JSON.parse(localStorage.getItem("user_details"));
-    this.amountBreakDown = [{
+    this.amountSum = [{
       value: 0
     }]
     this.userDetails.map(details => {
       this.monthly = details.amount
     })
-    this.amountBreakDown.map(amount => {
+    this.amountSum.map(amount => {
       amount.value = this.monthly
     })
     const headerCanvasSettings = {
@@ -240,6 +254,199 @@ export default {
         rotation: "40"
       }
     );
+    if(this.monthly <= 20000 ) {
+      TweenMax.fromTo(
+        ".the-money-monk",
+        0.3,
+        { opacity: "0" },
+        {
+          repeat: -1,
+          yoyo: true,
+          repeatDelay: 0.5,
+          ease: Linear.easeNone,
+          display: "block",
+          opacity: "1"
+        }
+      )
+      TweenMax.fromTo(
+        ".the-money-monk-comment",
+        0.3,
+        { opacity: "0" },
+        {
+          display: "block",
+          opacity: "1"
+        }
+      )
+    }
+    if(this.monthly > 20000 && this.monthly <= 30000) {
+      TweenMax.fromTo(
+        ".the-essentialist",
+        0.3,
+        { opacity: "0" },
+        {
+          repeat: -1,
+          yoyo: true,
+          repeatDelay: 0.5,
+          ease: Linear.easeNone,
+          display: "block",
+          opacity: "1"
+        }
+      )
+      TweenMax.fromTo(
+        ".the-essentialist-comment",
+        0.3,
+        { opacity: "0" },
+        {
+          display: "block",
+          opacity: "1"
+        }
+      )
+    }
+    if(this.monthly > 30000 && this.monthly <= 50000) {
+      TweenMax.fromTo(
+        ".the-sensible-sage",
+        0.3,
+        { opacity: "0" },
+        {
+          repeat: -1,
+          yoyo: true,
+          repeatDelay: 0.5,
+          ease: Linear.easeNone,
+          display: "block",
+          opacity: "1"
+        }
+      )
+      TweenMax.fromTo(
+        ".the-sensible-sage-comment",
+        0.3,
+        { opacity: "0" },
+        {
+          display: "block",
+          opacity: "1"
+        }
+      )
+    }
+    if(this.monthly > 50000 && this.monthly <= 60000) {
+      TweenMax.fromTo(
+        ".the-baby-baller",
+        0.3,
+        { opacity: "0" },
+        {
+          repeat: -1,
+          yoyo: true,
+          repeatDelay: 0.5,
+          ease: Linear.easeNone,
+          display: "block",
+          opacity: "1"
+        }
+      )
+      TweenMax.fromTo(
+        ".the-baby-baller-comment",
+        0.3,
+        { opacity: "0" },
+        {
+          display: "block",
+          opacity: "1"
+        }
+      )
+    }
+    if(this.monthly > 60000 && this.monthly <= 70000) {
+      TweenMax.fromTo(
+        ".the-spontanious-spender",
+        0.3,
+        { opacity: "0" },
+        {
+          repeat: -1,
+          yoyo: true,
+          repeatDelay: 0.5,
+          ease: Linear.easeNone,
+          display: "block",
+          opacity: "1"
+        }
+      )
+      TweenMax.fromTo(
+        ".the-spontanious-spender-comment",
+        0.3,
+        { opacity: "0" },
+        {
+          display: "block",
+          opacity: "1"
+        }
+      )
+    }
+    if(this.monthly > 70000 && this.monthly <= 85000) {
+      TweenMax.fromTo(
+        ".the-big-spender",
+        0.3,
+        { opacity: "0" },
+        {
+          repeat: -1,
+          yoyo: true,
+          repeatDelay: 0.5,
+          ease: Linear.easeNone,
+          display: "block",
+          opacity: "1"
+        }
+      )
+      TweenMax.fromTo(
+        ".the-big-spender-comment",
+        0.3,
+        { opacity: "0" },
+        {
+          display: "block",
+          opacity: "1"
+        }
+      )
+    }
+    if(this.monthly > 85000 && this.monthly <= 100000) {
+      TweenMax.fromTo(
+        ".the-generous-sage",
+        0.3,
+        { opacity: "0" },
+        {
+          repeat: -1,
+          yoyo: true,
+          repeatDelay: 0.5,
+          ease: Linear.easeNone,
+          display: "block",
+          opacity: "1"
+        }
+      )
+      TweenMax.fromTo(
+        ".the-generous-sage-comment",
+        0.3,
+        { opacity: "0" },
+        {
+          display: "block",
+          opacity: "1"
+        }
+      )
+    }
+    if(this.monthly > 100000 ) {
+      TweenMax.fromTo(
+        ".the-money-maestro",
+        0.3,
+        { opacity: "0" },
+        {
+          repeat: -1,
+          yoyo: true,
+          repeatDelay: 0.5,
+          ease: Linear.easeNone,
+          display: "block",
+          opacity: "1"
+        }
+      )
+      TweenMax.fromTo(
+        ".the-money-maestro-comment",
+        0.3,
+        { opacity: "0" },
+        {
+          display: "block",
+          opacity: "1"
+        }
+      )
+    }
+
   },
   methods: {
     calcAmount() {
@@ -249,19 +456,19 @@ export default {
       })
       if(this.selected === "Weekly") {
         this.weekly = amount / 4
-        this.amountBreakDown.map(amount => {
+        this.amountSum.map(amount => {
           amount.value = this.weekly
         })
       }
       if(this.selected === "Monthly") {
         this.monthly = amount
-        this.amountBreakDown.map(amount => {
+        this.amountSum.map(amount => {
           amount.value = this.monthly
         })
       }
       if(this.selected === "Yearly") {
         this.yearly = amount * 12
-        this.amountBreakDown.map(amount => {
+        this.amountSum.map(amount => {
           amount.value = this.yearly
         })
       }
